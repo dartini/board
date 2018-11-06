bfec3c2e75743db13657c029323c84269e977d41
Thomas Nisole <thomasnisole@gmail.com>
2018-11-06 19:54:04 +0100

    Add firebase configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {CoreModule} from './module/core/core.module';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
     BrowserModule,
     CoreModule
   ],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDd6qh89OUCNJYH8ZTohB4nlCIKjuek3B8',
+    authDomain: 'dartini-5eb20.firebaseapp.com',
+    databaseURL: 'https://dartini-5eb20.firebaseio.com',
+    projectId: 'dartini-5eb20',
+    storageBucket: 'dartini-5eb20.appspot.com',
+    messagingSenderId: '1069871766376'
+  }
 };
-
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
