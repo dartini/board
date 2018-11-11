83f2b050d8a13df5729dcd0e27588ec3280bd4bb
Thomas Nisole <thomasnisole@gmail.com>
2018-11-11 13:42:54 +0100

    Simplify target request observable

--- src/app/module/core/service/target.service.ts
+++ src/app/module/core/service/target.service.ts
@@ -1,8 +1,8 @@
 import {NgxTsDeserializerService} from 'ngx-ts-serializer';
 import {mergeMap, toArray, map} from 'rxjs/operators';
-import {Target} from './../model/target.model';
-import {User} from './../model/user.model';
-import {Observable, from} from 'rxjs';
+import {Target} from '../model/target.model';
+import {User} from '../model/user.model';
+import {Observable, from, forkJoin} from 'rxjs';
 import {AngularFirestore, QuerySnapshot, QueryDocumentSnapshot} from 'angularfire2/firestore';
 import {Injectable} from '@angular/core';
 
@@ -15,14 +15,13 @@ export class TargetService {
 
   public findUserTargets(user: User): Observable<Target[]> {
     return from(user.targetsNames).pipe(
-      mergeMap((name: string) => this.db.collection('targets', (ref) => ref.where('name', '==', name)).get()),
+      map((name: string) => this.db.collection('targets', (ref) => ref.where('name', '==', name)).get()),
       toArray(),
-      mergeMap((qss: QuerySnapshot<any>[]) => from(qss).pipe(
-        mergeMap((qs: QuerySnapshot<QueryDocumentSnapshot<any>>) => from(qs.docs).pipe(
-            map((r: QueryDocumentSnapshot<any>) => this.deserializer.deserialize(Target, r.data()))
-        )),
-        toArray()
-      )),
+      mergeMap((qss$: Observable<QuerySnapshot<any>>[]) => forkJoin(qss$)),
+      mergeMap((qss: QuerySnapshot<any>[]) => qss),
+      mergeMap((qs: QuerySnapshot<any>) => qs.docs),
+      map((qds: QueryDocumentSnapshot<any>) => this.deserializer.deserialize(Target, qds.data())),
+      toArray()
     );
   }
 
